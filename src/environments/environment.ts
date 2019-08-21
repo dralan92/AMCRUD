dcc2cfae6a1d57d165f473e55aab8b67bbf2a6f5
Alan Sebastian <alansebastian4@gmail.com>
2019-08-21 00:09:40 -0400

    Initial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDdrqwN3pt3p_e6ODw4fuVzs6sbPDqqfbk",
+    authDomain: "amcrud-d4ee1.firebaseapp.com",
+    databaseURL: "https://amcrud-d4ee1.firebaseio.com",
+    projectId: "amcrud-d4ee1",
+    storageBucket: "",
+    messagingSenderId: "589083576307",
+    appId: "1:589083576307:web:f3a96d847d7ab0b0"
+  }
 };
 
 /*
